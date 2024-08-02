77759bcb3473ea1e2d06f439daf27f635a6e2e30
Synx-Chillz <97074586+Synx-dev@users.noreply.github.com>
2024-08-02 11:22:22 +0700

    Update Source.lua
    Heheheheehe

--- Source.lua
+++ Source.lua
@@ -1,3 +1,32 @@
-print("placeholder")
-wait(3)
-game:GetService("Players").LocalPlayer:Kick("test")
+print("Loaded! Rspy Modified for celery!!")
+
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local function sendNotification(title, message, duration)
+    local success, errorMessage = pcall(function()
+        StarterGui:SetNotification({
+            Title = title,
+            Text = message,
+            --Icon = "rbxassetid://123456789", 
+            Duration = duration, 
+        })
+    end)
+    if not success then
+        warn("Error sending notification: ".. errorMessage)
+    end
+end
+
+local function kickPlayer(reason)
+    local success, errorMessage = pcall(function()
+        player:Kick(reason)
+    end)
+    if not success then
+        warn("Error loading Rspy Modified".. errorMessage)
+    end
+end
+
+sendNotification("Rspy UwU", "This might take long, i havent fix the performance issue yet ;-;", 5)
+task.wait(3)
+kickPlayer("Heheheheheheh, logged")
